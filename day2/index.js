04a54396f82ca9306e37edb0f86a3aa99905799f
Shrutarshi Ray <84886266+shrutarshi474@users.noreply.github.com>
2024-02-19 15:55:03 +0530

    add me

---
+++ day2/index.js
@@ -0,0 +1,120 @@
+// const elem1 = Number(prompt("enter the first number: "));
+// const elem2 = Number(prompt("enter the second number: "));
+// const name = ""
+// var sum = elem1 + elem2;
+// Data type
+// Number
+// let a = 16;
+// let b = 5.5;
+// // string
+// let name = "ray";
+// let word = "A";
+// // Booleans
+// let x = true;
+// let y = false;
+// // object
+// let person = {
+//   name: "Ray",
+//   stream: "CSE",
+//   year: 4,
+// };
+
+// // Object.element
+// // let{element,element} = object
+// person.name;
+// let { name, stream, year } = person;
+// console.log(stream);
+// let { stream, year } = person1;
+// console.log(stream);
+
+// // Array Object
+// // const cars = ["bmw", "nano", "odi"];
+// // new object
+// const date = new Date("2024-02-15");
+
+// // if else
+// const autofare = 50;
+// let usermoney = 25;
+
+// if (usermoney == autofare) {
+//   console.log("you can travel..");
+// } else if (usermoney > autofare) {
+//   console.log("yes you can travel bz you have more money..");
+// } else {
+//   console.log("you can't travel...");
+// }
+
+// //loop
+// // for loop
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// // list
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 87];
+// for (x in numbers) {
+//   console.log(x);
+// }
+
+// for (let y of numbers) {
+//   console.log(y);
+// }
+
+// //while loop
+// let i = 0;
+// while (i < 10) {
+//   document.write(i);
+//   i++;
+// }
+
+// // do while
+// let j = 0;
+// do {
+//   document.write(i);
+//   j++;
+// } while (i < 10);
+
+// // function
+// // function myfun(a, b) {
+// //   return a + b;
+// // }
+// // console.log(myfun(2, 3));
+
+// // let myfun = (a, b) => a + b;
+
+// // console.log(my)
+// // let age = "22";
+// // console.log(typeof age);
+// // let ageupdate = Number(age);
+// // console.log(typeof ageupdate);
+
+// // // myfun = () => {
+// // //   console.log("hello world");
+// // //   console.log("hello world");
+// // // };
+
+// // // class
+// class Person {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//     this.stream = stream;
+//   }
+// }
+
+// const person1 = new Person("ray", "4th", "CSE");
+// const person2 = new Person("ray1", "5th", "CSE");
+
+// // const elem = document.querySelector(".head");
+// // elem.innerHTML = Car1.name + Car1.year;
+// // Array Methods
+// const cars = ["BMW", "audi", "nano", "ford"];
+// console.log(cars.length);
+// cars.pop();
+// cars.push("honda");
+// cars[0] = "newcar";
+// console.log(cars.indexOf("nano3"));
+
+// document.querySelector("#head").innerHTML = cars;
+
+/
